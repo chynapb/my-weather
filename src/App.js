7dee620e74c2b4e6df02fc672b10a90c148e6a88
Chyna <chynapblair@gmail.com>
2023-09-03 09:55:09 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='app'>
-      <div className='container'>
-        <h1 className='header'>MyWeather</h1>
-      </div>
+    <div className='container'>
+      <h1 className='header'>MyWeather</h1>
     </div>
   );
 }
