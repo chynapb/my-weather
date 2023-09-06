564058c38bac620fda8f61db79adb675f1ca4738
Chyna <chynapblair@gmail.com>
2023-09-05 20:18:36 -0400

    Styling

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,14 +1,12 @@
 export const Forecast = () => {
   return (
     <div className='forecast'>
-      <h4>4 Day Forecast</h4>
+      <h4>5 Day Forecast</h4>
       <img src={'/imgs/sunny.png'} className='forecast-img'></img>
       <img src={'/imgs/partly-cloudy.png'} className='forecast-img'></img>
       <img src={'/imgs/cloudy.png'} className='forecast-img'></img>
       <img src={'/imgs/rain.png'} className='forecast-img'></img>
       <img src={'/imgs/snow.png'} className='forecast-img'></img>
-      <img src={'/imgs/partly-cloudy-moon.png'} className='forecast-img'></img>
-      <img src={'/imgs/moon.png'} className='forecast-img'></img>
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,6 @@ export const Search = () => {
   return (
     <div className='search'>
       <input id='search-box' type='text' placeholder='Enter your city...' />
-      <button>Search</button>
     </div>
   );
 };
