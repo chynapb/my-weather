e26d9c8be9076d0125c827a292c8aafe02c191ea
Chyna <chynapblair@gmail.com>
2023-09-10 18:48:36 -0400

    Styling

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -3,26 +3,31 @@ export const Forecast = () => {
     <div className='forecast'>
       <di>
         <h3>Sat</h3>
+        <img src={'/imgs/sunny.png'} alt='' />
         <p>Sunny</p>
         <p>60°F</p>
       </di>
       <di>
         <h3>Sun</h3>
+        <img src={'/imgs/rain.png'} alt='' />
         <p>Rain</p>
         <p>70°F</p>
       </di>
       <di>
         <h3>Mon</h3>
+        <img src={'/imgs/rain.png'} alt='' />
         <p>Rain</p>
         <p>67°F</p>
       </di>
       <di>
         <h3>Tue</h3>
+        <img src={'/imgs/cloudy.png'} alt='' />
         <p>Cloudy</p>
         <p>75°F</p>
       </di>
       <div>
         <h3>Wed</h3>
+        <img src={'/imgs/sunny.png'} alt='' />
         <p>Sunny</p>
         <p>87°F</p>
       </div>
